3dc87e626eed81c96499270d8a78045a9518ccd3
vita2333 <lp_vitadolce@163.com>
2024-10-16 11:45:59 +0800

    添加表达式的异步执行

--- addons/pindot/util/expression_parser.gd
+++ addons/pindot/util/expression_parser.gd
@@ -5,22 +5,29 @@ class_name ExpressionParser
 
 ## 函数映射表, 格式: {函数名:Callable}, 需要将函数封装成Callable
 ##		TIP: 函数定义时要将caller作为第一个参数,其他随意
-##		如: func	buy(caller:Customer,buyer:Customer):->void
+##		如: func	   buy(caller:Customer,buyer:Customer):->void
 var callable_map: Dictionary
 ## 存储每个函数的预期参数类型, 用于检验填写的表达式参数
 var callable_param_types: Dictionary
 
 
 ## 注册函数,只有注册过的函数才可以通过表达式调用
+## TIP:
+## 参数格式:
+##     - TYPE_NIL 匹配未指定类型的参数
+##     - TYPE_STRING
+##     - TYPE_INT 直接通过excel调用的函数是无法传入int格式的参数的,只有将其他函数的结果作为参数的情况,才能传入int格式
 ## 示例:
-## func testfunc1(caller: Variant, p):
-## 		return int(p) + 2
-## parser.register_callable("testfunc1", testfunc1, [TYPE_NIL])  # TYPE_NIL 匹配未指定类型的参数
+## func	testfunc1(caller: Variant, p):
+##		return int(p) +	2
+## parser.register_callable("testfunc1", testfunc1,	[TYPE_NIL])	 # TYPE_NIL	匹配未指定类型的参数
 ##
 ## func testfunc5(caller: Variant, p1: String, p2: int, p3: Array):
 ## 		return caller
 ## parser.register_callable("testfunc5", testfunc5, [TYPE_STRING, TYPE_INT, TYPE_ARRAY])
 func register_callable(key: String, callable: Callable, param_types: Array):
+	# callable 的 object 需要在类中显示声明,否则会invalid
+	assert(callable.is_valid(), "Callable is not valid: " + key)
 	assert(!callable_map.has(key), "Callable already registered: " + key)
 	callable_map[key] = callable
 	callable_param_types[key] = param_types
@@ -49,25 +56,32 @@ class ExpressionObj:
 ## 执行表达式
 ## @param caller 调用函数者,可以在函数中声音具体的类型, 如: function(caller:Customer)
 ## @param expression 字符串的表达式,如:	buy(nearest(["8001","8002"]))
-## 执行失败返回null
+## 返回函数结果
+##
+## TIP: 如果需要获取函数结果, 调用时需要使用await关键字,即便函数本身不是async函数
+## @link https://docs.godotengine.org/zh-cn/4.x/tutorials/scripting/gdscript/gdscript_basics.html#awaiting-signals-or-coroutines
+## 		如果你不需要结果，直接异步调用就可以了，既不会阻止代码的正常运行，也不会让当前的函数变成协程函数
+## 		对不是信号和协程函数的表达式使用 await，则会立即返回对应的值，函数也不会将控制权转交回调用方
 func execute_expression(caller: Variant, expression: String) -> Variant:
+	if expression == "":
+		return null
 	var obj = parse_expression(expression)
-	return call_method_recursive(caller, obj)
+	return await _call_method_recursive(caller, obj)
 
 
 ## 解析表达式的函数名和参数,返回ExpressionObj
 ## 示例:
 ## 输入: 'myFunction("param1", ["8001", "8002"], "testparam2")'
-## 输出: ["param1", ["8001", "8002"],	"testparam2"]
+## 输出: ["param1", ["8001", "8002"],	  "testparam2"]
 ## 输入: 'buy(nearest(["8001", "8002"], "testparam2"))'
 ## 输出: [ExpressionObj("buy", [ExpressionObj("nearest", [["8001", "8002"], "testparam2"])])]
-func parse_expression(input: String) -> ExpressionObj:
+func parse_expression(expression: String) -> ExpressionObj:
 	# 定义正则表达式，匹配函数名和所有参数部分
 	var regex = RegEx.new()
 	regex.compile(r"(\w+)\s*\((.*)\)")  # 匹配函数名和参数部分，允许空格
 
 	# 匹配输入字符串
-	var result = regex.search(input)
+	var result = regex.search(expression)
 
 	if result:
 		var method_name = result.get_string(1)  # 获取函数名
@@ -79,7 +93,7 @@ func parse_expression(input: String) -> ExpressionObj:
 		return ExpressionObj.new(method_name, params)
 
 	# 无需解析,直接原样传出
-	return ExpressionObj.new(input, [])
+	return ExpressionObj.new(expression, [])
 
 
 ## 分割参数，保留数组参数和嵌套函数调用
@@ -147,10 +161,14 @@ func _parse_array_param(param: String) -> Array:
 
 
 ## 递归解析并调用函数
-func call_method_recursive(caller: Variant, method_obj: ExpressionObj) -> Variant:
+func _call_method_recursive(caller: Variant, obj: ExpressionObj) -> Variant:
 	# 获取函数名并在映射表中查找对应的可调用函数
-	var func_name = method_obj.name
-	var callable: Callable = callable_map.get(func_name, null)
+	var func_name = obj.name
+
+	assert(callable_map.has(func_name), "Function not found: " + func_name)
+	var callable: Callable = callable_map[func_name]
+	# callable 的 object	需要在类中显示声明,否则会invalid
+	assert(callable.is_valid(), "callable not valid:" + func_name)
 
 	# 如果函数不存在，抛出错误
 	if not callable:
@@ -159,10 +177,10 @@ func call_method_recursive(caller: Variant, method_obj: ExpressionObj) -> Varian
 
 	# 解析并准备参数
 	var resolved_params = []
-	for param in method_obj.params:
+	for param in obj.params:
 		# 如果参数是嵌套的 ExpressionObj，则递归解析并调用
 		if param is ExpressionObj:
-			resolved_params.append(call_method_recursive(caller, param))
+			resolved_params.append(await _call_method_recursive(caller, param))
 		else:
 			# 否则直接添加原始参数
 			resolved_params.append(param)
@@ -178,8 +196,7 @@ func call_method_recursive(caller: Variant, method_obj: ExpressionObj) -> Varian
 	# 函数定义时要将caller作为第一个参数,其他参数由函数自定义
 	resolved_params.insert(0, caller)
 
-	# 调用函数并返回结果
-	return callable.callv(resolved_params)
+	return await callable.callv(resolved_params)
 
 
 # 在调用前检查参数类型
@@ -192,18 +209,22 @@ func _check_parameter_types(func_name: String, received_params: Array) -> bool:
 
 	# 不做参数个数校验,因为有的参数默认有值可以不填写
 	# if expected_params_count != received_params_count:
-	# 	# 输出: Parameter count mismatch in function: buy Expected: 1, Received: 0
-	# 	Pindot.log.error(
-	# 		"Parameter count mismatch in function: " + func_name,
-	# 		"Expected: " + str(expected_params_count) + ", Received: " + str(received_params_count)
-	# 	)
-	# 	return false
+	#	# 输出: Parameter	count mismatch in function:	buy	Expected: 1, Received: 0
+	#	Pindot.log.error(
+	#		"Parameter count mismatch in function: " + func_name,
+	#		"Expected: " + str(expected_params_count) +	", Received: " + str(received_params_count)
+	#	)
+	#	return false
+
 	for i in range(expected_params_count):
+		if i >= received_params_count:
+			# 实际接收参数可能会比预期少,因为带默认值的参数可以不填
+			break
 		# TYPE_NIL 这里代表任意类型
 		if expected_types[i] != TYPE_NIL and expected_types[i] != typeof(received_params[i]):
-			# 输出: Parameter mismatch in function: buy param1: expected Object, but got String "me"
+			# 输出: Parameter	mismatch in function: buy param1: expected Object, but got String "me"
 			Pindot.log.error(
-				"Parameter mismatch in function: " + func_name,
+				"Parameter mismatch	in function: " + func_name,
 				(
 					"param"
 					+ str(i + 1)
@@ -211,7 +232,7 @@ func _check_parameter_types(func_name: String, received_params: Array) -> bool:
 					+ _get_variant_type_name(expected_types[i])
 					+ ", but got "
 					+ _get_variant_type_name(typeof(received_params[i]))
-					+ ' "'
+					+ '	"'
 					+ str(received_params[i])
 					+ '"'
 				)
--- test/test_util/test_expression_parser.gd
+++ test/test_util/test_expression_parser.gd
@@ -30,12 +30,39 @@ func testfunc5(caller: Variant, p1: String, p2: int, p3: Array):
 	return caller
 
 
+func async_testfunc(caller: Variant, p1: String):
+	var res = int(p1)
+	for i in 2:
+		await wait_seconds(0.1)
+		res += 1
+		Pindot.log.info(
+			"async_testfunc awaiting", "res=" + str(res) + " time=" + str(Time.get_ticks_msec())
+		)
+	return res
+
+
+var async_testfunc1_data := 0
+
+
+func async_testfunc1(caller: Variant, p1: String):
+	for i in 2:
+		await wait_seconds(0.1)
+		async_testfunc1_data += int(p1)
+		Pindot.log.info(
+			"async_testfunc1 awaiting",
+			"res=" + str(async_testfunc1_data) + " time=" + str(Time.get_ticks_msec())
+		)
+	return async_testfunc1_data
+
+
 func before_all():
 	parser.register_callable("testfunc1", testfunc1, [TYPE_NIL])  # TYPE_NIL 匹配未指定类型的参数
 	parser.register_callable("testfunc2", testfunc2, [TYPE_STRING])
-	parser.register_callable("testfunc3", testfunc3, [TYPE_INT, TYPE_INT])
+	parser.register_callable("testfunc3", testfunc3, [TYPE_INT, TYPE_INT])  # 直接通过excel调用的函数是无法传入int格式的参数的,只有将其他函数的结果作为参数的情况,才能传入int格式
 	parser.register_callable("testfunc4", testfunc4, [TYPE_NIL])
 	parser.register_callable("testfunc5", testfunc5, [TYPE_STRING, TYPE_INT, TYPE_ARRAY])
+	parser.register_callable("async_testfunc", async_testfunc, [TYPE_STRING])
+	parser.register_callable("async_testfunc1", async_testfunc1, [TYPE_STRING])
 
 
 #endregion
@@ -118,29 +145,28 @@ func test_nested_func_with_params():
 
 #region 函数调用
 
+
 ## 函数定义时要将caller作为第一个参数,其他随意
-
-
 func test_检查函数调用结果():
-	var result = parser.execute_expression(self, "testfunc1(testfunc2(1))")
+	var result = await parser.execute_expression(self, "testfunc1(testfunc2(1))")
 	Pindot.log.info("testfunc1(testfunc2(1)) result ====", result)
 	assert_true(result == 4, "result should be 4, but: " + str(result))
 
 
 func test_检查函数调用结果_多参数():
-	var result = parser.execute_expression(self, "testfunc3(testfunc1(1),testfunc2(1))")
+	var result = await parser.execute_expression(self, "testfunc3(testfunc1(1),testfunc2(1))")
 	Pindot.log.info("testfunc3(testfunc1(1),testfunc2(1)) result ====", result)
 	assert_true(result == 5, "result should be 5, but: " + str(result))
 
 
 func test_可以传入额外的参数():
-	var result = parser.execute_expression(TestClass.new(), "testfunc4(testfunc1(1))")
+	var result = await parser.execute_expression(TestClass.new(), "testfunc4(testfunc1(1))")
 	Pindot.log.info("testfunc4(testfunc1(1)) result ====", result)
 	assert_true(result is TestClass, "result wrong: " + str(result))
 
 
 func test_参数数量不对报错():
-	var result = parser.execute_expression(TestClass.new(), "testfunc4(testfunc1(1),1,2,3)")
+	var result = await parser.execute_expression(TestClass.new(), "testfunc4(testfunc1(1),1,2,3)")
 	assert_true(result == null)
 	Pindot.log.info("这里会有报错↑↑↑")
 
@@ -149,11 +175,36 @@ func test_参数数量不对报错():
 
 
 func test_参数类型不对报错():
-	var result = parser.execute_expression(self, "testfunc5(1,2,3)")
+	var result = await parser.execute_expression(self, "testfunc5(1,2,3)")
 	assert_true(result == null)
 	Pindot.log.info("这里会有报错↑↑↑")
 
 	parser.execute_expression(self, 'testfunc5("string",testfunc1(1),[3,4])')
 	Pindot.log.info("这里没有报错↑↑↑")
+
+
+func test_异步函数调用():
+	var result = await parser.execute_expression(self, "async_testfunc(1)")
+	Pindot.log.info("async_testfunc(1) result ====", result)
+
+	assert_true(result == 3, "result should be 3, but: " + str(result))
+
+
+func test_可以直接调用异步函数():
+	parser.execute_expression(self, "async_testfunc1(100)")
+	Pindot.log.info("直接执行,函数会异步调用")
+	assert_true(
+		async_testfunc1_data == 0,
+		"async_testfunc1_data should be 0, but: " + str(async_testfunc1_data)
+	)
+	await wait_seconds(0.1)
+	await wait_seconds(0.1)
+	Pindot.log.info("这里有结果了↑↑↑")
+	
+	Pindot.log.info("async_testfunc(1) result ====", async_testfunc1_data)
+	assert_true(
+		async_testfunc1_data == 200,
+		"async_testfunc1_data should be 200, but: " + str(async_testfunc1_data)
+	)
 
 #endregion
